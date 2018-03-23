c5625fb246753bb19e79fc99c5b04985baa69859
razPasca <raz_pasca@yahoo.com>
2018-03-23 22:00:01 +0200

    Connected to the database

---
+++ Code/Project1/src/connection/DatabaseConnection.java
@@ -0,0 +1,79 @@
+/**
+ *
+ */
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * @author pasca
+ *
+ */
+public class DatabaseConnection {
+    private static final String DRIVER ="com.mysql.jdbc.Driver";
+    private static final String DBURL ="jdbc:mysql://localhost:3306/hybridrpg_db?autoReconnect=true&useSSL=false";
+    private static final String USER= "root";
+    private static final String PASS="root";
+
+    private static DatabaseConnection Instance = new DatabaseConnection();
+
+    private DatabaseConnection(){
+        try {
+            Class.forName(DRIVER);
+            System.out.println("connected successfully");
+        }
+        catch (ClassNotFoundException e){
+            System.out.println("connection failed");
+            e.printStackTrace();
+        }
+    }
+
+    private Connection createConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(DBURL, USER, PASS);
+        } catch (SQLException e) {
+            System.out.println( "An error occured while trying to connect to the database");
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static Connection getConnection() {
+        return Instance.createConnection();
+    }
+
+    public static void close(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                System.out.println( "An error occured while trying to close the connection");
+            }
+        }
+    }
+
+    public static void close(Statement statement) {
+        if (statement != null) {
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                System.out.println( "An error occured while trying to close the statement");
+            }
+        }
+    }
+
+    public static void close(ResultSet resultSet) {
+        if (resultSet != null) {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                System.out.println( "An error occured while trying to close the ResultSet");
+            }
+        }
+    }
+}
---
+++ Code/Project1/src/test/Main.java
@@ -0,0 +1,60 @@
+package test;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import connection.DatabaseConnection;
+
+import java.sql.ResultSet;
+
+
+
+public class Main {
+
+    public static void main(String args[]) {
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        Connection conn = DatabaseConnection.getConnection();
+
+        try {
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("SELECT * FROM app_user");
+
+            // or alternatively, if you don't know ahead of time that
+            // the query will be a SELECT...
+            System.out.println(rs.getFetchSize());
+
+            // Now do something with the ResultSet ....
+        }
+        catch (SQLException ex){
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+        finally {
+            // it is a good idea to release
+            // resources in a finally{} block
+            // in reverse-order of their creation
+            // if they are no-longer needed
+
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException sqlEx) { } // ignore
+
+                rs = null;
+            }
+
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException sqlEx) { } // ignore
+
+                stmt = null;
+            }
+        }
+    }
+}
